f348c29359ea698ffc2258536bc0f99a32074084
Fikir101 <fikirjunior19@gmail.com>
2022-12-08 12:00:01 -0800

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - prints exactly and that peace of art is useful
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that peace of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+	return (1);
+}
