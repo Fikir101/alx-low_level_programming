4482a04f1b3b5e4ef860f29b373da55e85908bf0
Fikir101 <fikirjunior19@gmail.com>
2022-12-08 10:48:13 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: always 0
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n")
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
