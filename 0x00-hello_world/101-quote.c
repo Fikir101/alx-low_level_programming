fb9536844fc2d39c0f58fcf4bb4ca3aee16707fa
Fikir101 <fikirjunior19@gmail.com>
2022-12-08 12:27:57 -0800

    Advanced Task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
  * main - prints exactly and that peace of art is useful
  * Return: always 0 (Success)
@@ -6,5 +7,6 @@
 int main(void)
 {
 	write(2, "and that peace of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fprintf("Error");
 	return (1);
 }
