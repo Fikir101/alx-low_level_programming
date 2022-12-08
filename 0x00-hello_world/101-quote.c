06628abee2e0fd3d66645ccb11ed009a2b5f0942
Fikir101 <fikirjunior19@gmail.com>
2022-12-08 12:41:40 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 /**
  * main - prints exactly and that peace of art is useful
- * Return: always 0 (Success)
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that peace of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that peace of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
