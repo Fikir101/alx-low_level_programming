cd037b4ed97de5fbea4d42cbaadbd72ea9908c7e
Fikir101 <fikirjunior19@gmail.com>
2022-12-09 12:40:15 -0800

    Task 2 and 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Prints alphabet in lower case
- * Return: Always 0
+ * main - returns alphabets
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char lc = 'a';
+	int lc = 'a';
 
 	while (lc <= 'z')
 	{
 		putchar(lc);
 		lc++;
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints alphabet in lower and upper case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
