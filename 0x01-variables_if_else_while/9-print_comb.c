22834e02971fc793c14e51fa95669464bcdaabf5
Fikir101 <fikirjunior19@gmail.com>
2022-12-09 14:37:06 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
-		putchar(n);
-		if (n != 57)
+	for (n = 0; n < 100; n++)
+	{
+		putchar(n + '0');
+		if (n < 99)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 
 	return (0);
