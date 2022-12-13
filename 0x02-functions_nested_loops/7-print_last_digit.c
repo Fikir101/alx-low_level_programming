9dd325dd736d22b8c31e0886d39e7153daf1023e
Fikir101 <fikirjunior19@gmail.com>
2022-12-13 12:55:58 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of agiven number
+ * @n: Checked number
+ * Return: some functions
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
