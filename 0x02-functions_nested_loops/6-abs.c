988c31dd713395ff59affa827480b685914f124a
Fikir101 <fikirjunior19@gmail.com>
2022-12-13 12:41:51 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Entry
+ * @n: checked integer
+ * Return: Absolute value of an integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
