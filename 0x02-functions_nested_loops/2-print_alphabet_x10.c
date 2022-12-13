8e195ec06d54d1eca0a2c53b072f0254fb51f4d8
Fikir101 <fikirjunior19@gmail.com>
2022-12-13 10:10:43 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 1;
+	char l;
+
+	while (count++ <= 10)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+			_putchar(l);
+	}
+
+	_putchar('\n');
+}
