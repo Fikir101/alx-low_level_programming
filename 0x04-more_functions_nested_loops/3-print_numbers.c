a21348d74b64e37bbbee83d68fe0a43ce94f126b
Fikir101 <fikirjunior19@gmail.com>
2022-12-15 11:09:09 -0800

    All mandatory Tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
