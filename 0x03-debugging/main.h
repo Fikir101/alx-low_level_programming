ea86295fc7b923b45d6a9b4f54c22e9a59242add
Fikir101 <fikirjunior19@gmail.com>
2022-12-15 08:02:00 -0800

    main.h library

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int m, int d);
+void print_remaining_days(int m, int d, int y);
+
+#endif
